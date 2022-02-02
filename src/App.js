e9a188ff52c255d71706d1b00a20cecbf8328e24
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-02 15:46:33 -0300

    adding react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ElectionResume from './views/ElectionResume.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ElectionResume></ElectionResume>
+    </div>
+  );
+}
+
+export default App;
