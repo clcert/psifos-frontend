cf7741c85d584124a5a124ec2e04a3c10d82289f
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-02 15:47:33 -0300

    Election Code component

---
+++ src/component/ElectionCode.js
@@ -0,0 +1,14 @@
+import "../static/css/booth.css";
+
+
+function ElectionCode() {
+  return (
+    <div class="container has-text-centered">
+      <p>CÓDIGO DE LA ELECCIÓN</p>
+      <p class="is-family-monospace">Codigo Eleccion</p>
+    </div>
+  );
+}
+
+
+export default ElectionCode;
