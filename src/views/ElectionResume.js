27f9005ba21f14143055c24433fa01a7eedd1fa3
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-03 16:41:28 -0300

    connect front-back for election resume

--- src/views/ElectionResume.js
+++ src/views/ElectionResume.js
@@ -1,99 +1,153 @@
+import { useState } from "react";
 import { Container, Image } from "react-bootstrap";
 import ElectionCode from "../component/ElectionCode";
 import MyNavbar from "../component/MyNavbar";
 import trustees from "../static/svg/trustees-list.svg";
 import "../static/css/booth.css";
+import { useParams } from "react-router";
+import backendIP from "../server";
 
 function ElectionResume() {
-  return (
-    <div id="content-voters">
-      <section class="parallax hero is-medium">
-        <div className="hero-body pt-0 px-0 header-hero">
-          <MyNavbar></MyNavbar>
-          <div
-            class="has-text-centered mt-6 py-3"
-            id="election-title-background"
-          >
-            <h1
-              class="title has-text-black is-size-4-mobile"
-              id="election-title"
+  const [nameElection, setNameElection] = useState("");
+  const [numVoters, setNumVoters] = useState("");
+  const [totalVoters, setTotalVoters] = useState("");
+  const [maxWeight, setMaxWeight] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [infoElection, setInfoElection] = useState(null);
+
+  const { uuid } = useParams();
+
+  async function getElectionResume() {
+    const resp = await fetch(
+      backendIP + "/elections/" + uuid + "/resume",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const jsonResponse = await resp.json();
+    return jsonResponse;
+  }
+  if (!loading) {
+    getElectionResume().then((data) => {
+      setNameElection(data.name);
+      setNumVoters(data.num_voters);
+      setTotalVoters(data.total_voters);
+      setMaxWeight(data.max_weight);
+      setInfoElection(data.info);
+      setLoading(true);
+    });
+  }
+
+  if (infoElection !== null) {
+    return (
+      <div id="content-voters">
+        <section className="parallax hero is-medium">
+          <div className="hero-body pt-0 px-0 header-hero">
+            <MyNavbar></MyNavbar>
+            <div
+              className="has-text-centered mt-6 py-3"
+              id="election-title-background"
             >
-              RESUMEN DE ELECCIÓN
-              <br />
-              wu
+              <h1
+                className="title has-text-black is-size-4-mobile"
+                id="election-title"
+              >
+                RESUMEN DE ELECCIÓN
+                <br />
+                {nameElection}
+              </h1>
+            </div>
+          </div>
+        </section>
+        <section className="section voters-section is-flex is-flex-direction-column is-align-items-center">
+          <div>
+            <h1 className="title is-size-4">Apertura de Urna</h1>
+          </div>
+
+          <div className="disable-text-selection row justify-content-md-center">
+            <table
+              id="resume-table"
+              className="mt-2 table is-bordered is-hoverable voters-table"
+            >
+              <tbody>
+                <tr>
+                  <td>Votos Recibidos</td>
+                  <td className="has-text-centered">{numVoters}</td>
+                </tr>
+                <tr>
+                  <td>Total Padrón</td>
+                  <td className="has-text-centered">{totalVoters}</td>
+                </tr>
+                <tr>
+                  <td>Participación</td>
+                  <td className="has-text-centered">
+                    {((numVoters / totalVoters) * 100).toFixed(2)}%
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <h1 className="title is-size-4 pt-4">
+              Número de votantes por ponderación
             </h1>
           </div>
-        </div>
-      </section>
-      <section class="section voters-section is-flex is-flex-direction-column is-align-items-center">
-        <div>
-          <h1 class="title is-size-4">Apertura de Urna</h1>
-        </div>
+          <div className="disable-text-selection row justify-content-md-center">
+            <table
+              id="weights-table"
+              className="mt-2 table is-bordered is-hoverable voters-table"
+            >
+              <thead>
+                <tr>
+                  <th>Ponderador</th>
+                  <th>Preliminar</th>
+                  <th>Inicial</th>
+                  <th>Votos Recibidos</th>
+                  <th>Efectivo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(infoElection).map((key) => (
+                  <tr key={key}>
+                    <td className="has-text-centered">{key / maxWeight}</td>
+                    <td className="has-text-centered"></td>
+                    <td className="has-text-centered"></td>
+                    <td className="has-text-centered"></td>
+                    <td className="has-text-centered">{infoElection[key]}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
 
-        <div className="disable-text-selection row justify-content-md-center">
-          <table
-            id="resume-table"
-            class="mt-2 table is-bordered is-hoverable voters-table"
-          >
-            <tr>
-              <td>Votos Recibidos</td>
-              <td class="has-text-centered">{1}</td>
-            </tr>
-            <tr>
-              <td>Total Padrón</td>
-              <td class="has-text-centered">{2}</td>
-            </tr>
-            <tr>
-              <td>Participación</td>
-              <td class="has-text-centered">{3}</td>
-            </tr>
-          </table>
-        </div>
-        <div>
-          <h1 class="title is-size-4 pt-4">
-            Número de votantes por ponderación
-          </h1>
-        </div>
-        <div className="disable-text-selection row justify-content-md-center">
-          <table
-            id="weights-table"
-            class="mt-2 table is-bordered is-hoverable voters-table"
-          >
-            <tr>
-              <th>Ponderador</th>
-              <th>Preliminar</th>
-              <th>Inicial</th>
-              <th>Votos Recibidos</th>
-              <th>Efectivo</th>
-            </tr>
-
-            <tr>
-              <td class="has-text-centered">{2}</td>
-              <td class="has-text-centered"></td>
-              <td class="has-text-centered"></td>
-              <td class="has-text-centered"></td>
-              <td class="has-text-centered">{2}</td>
-            </tr>
-          </table>
-        </div>
-      </section>
-
-      <section
-        class="section pb-0 is-flex is-justify-content-center is-align-content-flex-end"
-        id="drawing-section"
-      >
-        <figure class="image">
-          <Image id="send-final-img" src={trustees} />
-        </figure>
-      </section>
-      <footer class="footer">
-        <ElectionCode />
-      </footer>
-      <section class="hero">
-        <div class="hero-body bottom-hero"></div>
-      </section>
-    </div>
-  );
+        <section
+          className="section pb-0 is-flex is-justify-content-center is-align-content-flex-end"
+          id="drawing-section"
+        >
+          <figure className="image">
+            <Image id="send-final-img" src={trustees} />
+          </figure>
+        </section>
+        <footer className="footer">
+          <ElectionCode uuid={uuid} />
+        </footer>
+        <section className="hero">
+          <div className="hero-body bottom-hero"></div>
+        </section>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  }
 }
 
 export default ElectionResume;
