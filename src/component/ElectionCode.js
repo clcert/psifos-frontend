35a185ff251d3bc627dbb3b51d37a7e00997398f
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-07 17:21:02 -0300

    election code footer changes

--- src/component/ElectionCode.js
+++ src/component/ElectionCode.js
@@ -1,16 +1,21 @@
 import "../static/css/booth.css";
 
-
 function ElectionCode(props) {
   return (
-    <div className="container has-text-centered">
-      <p>CÓDIGO DE LA ELECCIÓN</p>
-      <div className="columns is-centered">
-      <p className="is-family-monospace">{props.uuid}</p>
-      </div>
+    <div>
+      <footer className="footer">
+        <div className="container has-text-centered">
+          <p>CÓDIGO DE LA ELECCIÓN</p>
+          <div className="columns is-centered">
+            <p className="is-family-monospace">{props.uuid}</p>
+          </div>
+        </div>
+      </footer>
+      <section className="hero">
+        <div className="hero-body bottom-hero"></div>
+      </section>
     </div>
   );
 }
-
 
 export default ElectionCode;
