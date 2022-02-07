b25b5ad32a9bdcb33db021fd829bbfdaf8499653
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-07 17:20:35 -0300

    adding footer participa

---
+++ src/component/FooterParticipa.js
@@ -0,0 +1,19 @@
+function FooterParticipa() {
+  return (
+    <div>
+      <footer class="footer">
+        <div class="container has-text-centered">
+          <p>
+            PARTICIPA.UCHILE es un proyecto de la Universidad de Chile - 2021
+          </p>
+        </div>
+      </footer>
+
+      <section className="hero">
+        <div className="hero-body bottom-hero"></div>
+      </section>
+    </div>
+  );
+}
+
+export default FooterParticipa;
