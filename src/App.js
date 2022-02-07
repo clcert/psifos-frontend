7aa7aac46c92294d98bc0680311b9127a7b920b9
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-07 17:22:36 -0300

    new route for custodio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import { Router, Routes, Route } from "react-router-dom";
 import ElectionResume from "./views/ElectionResume";
 import Urna from "./views/Urna";
+import CustodioClaves from "./views/CustodioClaves";
+
 
 function App() {
   return (
     <Routes>
       <Route path=":uuid/resumen" element={<ElectionResume/>} />
       <Route path=":uuid/urna" element={<Urna/>} />
+      <Route path=":uuid/custodio" element={<CustodioClaves/>} />
     </Routes>
   );
 }
