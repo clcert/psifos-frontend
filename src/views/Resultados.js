4fc969d4d9737c2b5e424fd68ac6af52d95bb0ae
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-09 16:47:06 -0300

    resultados view className

--- src/views/Resultados.js
+++ src/views/Resultados.js
@@ -11,16 +11,16 @@ function Resultados() {
   const { uuid } = useParams();
   return (
     <div id="content-results">
-      <section id="header-section" class="parallax hero is-medium">
-        <div class="hero-body pt-0 px-0 header-hero">
+      <section id="header-section" className="parallax hero is-medium">
+        <div className="hero-body pt-0 px-0 header-hero">
           <MyNavbar />
 
           <div
-            class="has-text-centered mt-6 py-3"
+            className="has-text-centered mt-6 py-3"
             id="election-title-background"
           >
             <h1
-              class="title has-text-black is-size-4-mobile"
+              className="title has-text-black is-size-4-mobile"
               id="election-title"
             >
               {electionName}
@@ -32,7 +32,7 @@ function Resultados() {
       </section>
 
       <section
-        class="section is-flex is-align-items-center is-flex-direction-column"
+        className="section is-flex is-align-items-center is-flex-direction-column"
         id="results-section"
       >
         {showResult ? (
@@ -40,16 +40,16 @@ function Resultados() {
             {prettyResult.map((question, index) => {
               return (
                 <>
-                  <div class="box" id="question-box-results">
+                  <div className="box" id="question-box-results">
                     <b>
-                      <span class="has-text-info">
+                      <span className="has-text-info">
                         Pregunta #{forLoop.counter}:{" "}
                       </span>
                       {question.question}
                     </b>
                     <br />
                   </div>
-                  <table class="table is-bordered mb-6" id="results-table">
+                  <table className="table is-bordered mb-6" id="results-table">
                     <tbody>
                       {question.answer.map((answer) => {
                         <tr>
@@ -68,8 +68,8 @@ function Resultados() {
             })}
           </>
         ) : (
-          <div class="box" id="not-results-box">
-            <p class="is-size-3 has-text-weight-bold">
+          <div className="box" id="not-results-box">
+            <p className="is-size-3 has-text-weight-bold">
               Resultados aún no calculados. Vuelve más tarde.
             </p>
           </div>
