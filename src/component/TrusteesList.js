c107d62f491b4b277b7b0b33fe2a2b778542b124
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-12 16:40:14 -0300

    trustees list component

---
+++ src/component/TrusteesList.js
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { backendIP } from "../server";
+import ConfirmAlert from "./ConfirmAlert";
+
+function TrusteesList(props) {
+  const [trustees, setTrustees] = useState([]);
+
+  useEffect(function effectFunction() {
+    async function getTrustees() {
+      const resp = await fetch(
+        backendIP + "/elections/" + props.uuid + "/trustees",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const jsonResponse = await resp.json();
+      setTrustees(jsonResponse);
+    }
+    getTrustees();
+  }, []);
+
+  return (
+    <div>
+      {!trustees ? (
+        <></> // TODO: Revisar condición
+      ) : (
+        <>
+          {trustees.map((t, index) => {
+            return (
+              <div className="box" id="trustee-box" key={t.name}>
+                <span className="has-text-weight-bold is-size-4">
+                  Custodio de Clave #{index + 1}: {t.name}
+                  {props.admin && (
+                    <>
+                      {t.secret_key ? (
+                        <>
+                          {!props.election.frozen_at && [
+                            <a
+                              id="trustees-link"
+                              onClick={() => {
+                                return window.confirm(
+                                  "Are you sure you want to remove Helios as a trustee?"
+                                );
+                              }}
+                              href=""
+                            >
+                              eliminar
+                            </a>,
+                          ]}
+                        </>
+                      ) : (
+                        <>
+                          <br />({t.email})<span> &nbsp; </span>
+                          {!props.election.frozen_at && (
+                            <>
+                              [
+                              <ConfirmAlert
+                                id="trustees-link"
+                                title={"eliminar"}
+                                message={
+                                  "Are you sure you want to remove this Trustee?"
+                                }
+                                href="google.com"
+                              />
+                              ]
+                            </>
+                          )}
+                          <span> &nbsp; </span>[
+                          <ConfirmAlert
+                            id="trustees-link"
+                            title={"enviar link"}
+                            message={
+                              "Are you sure you want to send this trustee his/her admin URL?"
+                            }
+                            href=""
+                          />
+                          ]
+                        </>
+                      )}
+                    </>
+                  )}
+                </span>
+
+                {t.public_key_hash ? (
+                  <p className="mt-4">
+                    Código de Clave Pública: <tt>{t.public_key_hash}</tt>
+                  </p>
+                ) : (
+                  <p className="mt-4">Custodio aún no sube su clave pública.</p>
+                )}
+
+                {props.election.encrypted_tally && (
+                  <p
+                    id="status-decryption-{{ forloop.counter0 }}"
+                    className="has-text-weight-bold is-size-5 status-decryption is-flex is-flex-direction-column"
+                  >
+                    ESPERANDO DESENCRIPTACIÓN PARCIAL
+                    <img
+                      className="is-align-self-center loading-trustee"
+                      src="{{ settings.STATIC_URL }}app/loading2.gif"
+                    />
+                  </p>
+                )}
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default TrusteesList;
