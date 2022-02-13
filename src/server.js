fcf4575fb2c3c4e571fc24cf76ccfd3b655a717d
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-02-13 14:08:21 -0300

    adding frontIP

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
 const backendIP = "http://localhost:5000";
 const backendHeliosIP = "http://localhost:8000";
-export { backendIP, backendHeliosIP };
+const frontIP = "http://localhost:3000";
+export { backendIP, backendHeliosIP, frontIP };
