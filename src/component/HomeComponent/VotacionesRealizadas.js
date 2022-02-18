6aeabdab02b3af69b66921122a69cb6cf46c7594
Tobal <jaramillo.cristobal.a@gmail.com>
2022-02-18 15:17:23 -0300

    votaciones realizadas get info from json

--- src/component/HomeComponent/VotacionesRealizadas.js
+++ src/component/HomeComponent/VotacionesRealizadas.js
@@ -1,70 +1,31 @@
-function VotacionesRealizadas() {
+function VotacionesRealizadas(props) {
+  const data = require("../../static/dummyData/eleccionesData.json");
   return (
     <div>
       <section className="section past-section">
         <div className="container">
-          <div className="past-elections-box pb-2">
-            <p className="election-info mb-2 mx-2">
-              <span className="has-text-weight-bold current-election-title">
-                ELECCIONES REALIZADAS
-              </span>
-            </p>
-            <p className="past-election">
-              14-12-2021 <span className="bullet-2">⦿</span> Facultad de
-              Ciencias Físicas y Matemáticas
-            </p>
-            <p className="past-election">
-              03-12-2021 <span className="bullet-1">⦿</span> Facultad de
-              Economía y Negocios
-            </p>
-            <p className="past-election">
-              17-11-2021 <span className="bullet-4">⦿</span> Instituto de
-              Estudios Internacionales
-            </p>
-            <p className="past-election">
-              11-11-2021 <span className="bullet-3">⦿</span> Asociación de
-              Funcionarios Servicios Centrales
-            </p>
-            <p className="past-election">
-              09-11-2021 <span className="bullet-2">⦿</span> Facultad de
-              Ciencias Químicas y Farmacéuticas
-            </p>
-            <p className="past-election">
-              09-11-2021 <span className="bullet-1">⦿</span> Facultad de
-              Ciencias
-            </p>
-            <p className="past-election">
-              02-11-2021 <span className="bullet-4">⦿</span> Facultad de
-              Arquitectura y Urbanismo
-            </p>
-            <p className="past-election">
-              20-10-2021 <span className="bullet-3">⦿</span> Instituto de
-              Estudios Avanzados en Educación
-            </p>
-            <p className="past-election">
-              05-10-2021 <span className="bullet-2">⦿</span> Facultad de
-              Ciencias Veterinarias y Pecuarias
-            </p>
-            <p className="past-election">
-              27-09-2021 <span className="bullet-1">⦿</span> Instituto de
-              Asuntos Públicos
-            </p>
-            <p className="past-election">
-              27-08-2021 <span className="bullet-4">⦿</span> Facultad de
-              Economía y Negocios
-            </p>
-            <p className="past-election">
-              25-08-2021 <span className="bullet-3">⦿</span> Facultad de
-              Ciencias Físicas y Matemáticas
-            </p>
-            <p className="past-election">
-              26-07-2021 <span className="bullet-2">⦿</span> Facultad de
-              Ciencias Sociales
-            </p>
-            <p className="past-election">
-              14-07-2021 <span className="bullet-1">⦿</span> Instituto de
-              Estudios Internacionales
-            </p>
+          <div className="past-elections-box  pb-2">
+            <div className="is-flex mt-2">
+              <img width={40} height={40} src={props.image} />
+              <p className="election-info mb-2 mx-2">
+                <span className="has-text-weight-bold current-election-title">
+                  {props.title}
+                </span>
+              </p>
+            </div>
+            <div className="content-past-elections pl-5">
+              {Object.keys(data).map((key, index) => {
+                return (
+                  <p className="past-election" key={index}>
+                    {data[key].Fecha}{" "}
+                    <span className={"bullet-" + String((index % 2) + 1)}>
+                      ●
+                    </span>{" "}
+                    {data[key].eleccion}
+                  </p>
+                );
+              })}
+            </div>
           </div>
         </div>
       </section>
