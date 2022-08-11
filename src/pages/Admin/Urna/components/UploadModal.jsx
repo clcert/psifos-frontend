a127f3bb23166080f0c4b55afb4b59280be79b49
Tobal <jaramillo.cristobal.a@gmail.com>
2022-08-10 21:56:42 -0400

    send to upload

--- src/pages/Admin/Urna/components/UploadModal.jsx
+++ src/pages/Admin/Urna/components/UploadModal.jsx
@@ -33,7 +33,7 @@ function UploadModal(props) {
       const input = document.getElementById("fileinput");
       data.append("file", input.files[0]);
       const token = sessionStorage.getItem("token");
-      const resp = await fetch(backendIP + "/" + props.uuid + "/send-voters", {
+      const resp = await fetch(backendIP + "/" + props.uuid + "/upload-voters", {
         method: "POST",
         headers: {
           Authorization: "Bearer " + token,
