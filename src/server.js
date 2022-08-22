3006d9b59c2028e0fb16e828a15c1e07e73e42c6
Tobal <jaramillo.cristobal.a@gmail.com>
2022-08-22 16:28:50 -0400

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
-const backendIP = process.env.APP_BACKEND_HOST || "http://localhost:8000";
-const backendHeliosIP = process.env.APP_BACKEND_HELIOS_HOST || "http://localhost:8000";
-const frontIP = process.env.APP_FRONTEND_HOST || "http://localhost:3000";
+let backendIP = "APP_BACKEND_HOST"
+let backendHeliosIP = "APP_BACKEND_HELIOS_HOST"
+let frontIP = "APP_FRONTEND_HOST"
+
+backendIP = backendIP !== "APP_BACKEND_HOST" ? backendIP : "http://localhost:8000"
+backendHeliosIP = backendHeliosIP !== "APP_BACKEND_HELIOS_HOST" ? backendHeliosIP : "http://localhost:8000"
+frontIP = frontIP !== "APP_FRONTEND_HOST" ? frontIP : "http://localhost:3000"
+
 export { backendIP, backendHeliosIP, frontIP };
