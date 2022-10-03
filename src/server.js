2053afe242919e7e4b2c9303bbed575a6d32c6d2
JaramilloCris <cristobal.jaramillo@ug.uchile.cl>
2022-10-03 16:55:54 -0300

    Change names env url

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-let backendOpIP = "APP_BACKEND_OP_HOST"
-let backendInfoIp = "APP_BACKEND_INFO_HOST"
-let frontIP = "APP_FRONTEND_HOST"
+let backendOpIP = "APP_BACKEND_OP_URL"
+let backendInfoIp = "APP_BACKEND_INFO_URL"
+let frontIP = "APP_FRONTEND_URL"
 
 
-backendOpIP = backendOpIP !== "APP_BACKEND_OP_HOST" ? backendOpIP : "http://localhost:8000/psifos/api/app"
-backendInfoIp = backendInfoIp !== "APP_BACKEND_INFO_HOST" ? backendInfoIp : "http://localhost:8001/psifos/api/public"
-frontIP = frontIP !== "APP_FRONTEND_HOST" ? frontIP : "http://localhost:3000/psifos"
+backendOpIP = backendOpIP !== "APP_BACKEND_OP_URL" ? backendOpIP : "http://localhost:8000/psifos/api/app"
+backendInfoIp = backendInfoIp !== "APP_BACKEND_INFO_URL" ? backendInfoIp : "http://localhost:8001/psifos/api/public"
+frontIP = frontIP !== "APP_FRONTEND_URL" ? frontIP : "http://localhost:3000/psifos"
 
 export { backendOpIP, backendInfoIp, frontIP };
