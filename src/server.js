d6c114cac044b7366219d6a2649cd402eba847f1
Tobal <jaramillo.cristobal.a@gmail.com>
2022-10-07 18:45:12 -0300

    dev without /psifos/app

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ let backendInfoIp = "APP_BACKEND_INFO_URL"
 let frontIP = "APP_FRONTEND_URL"
 
 
-backendOpIP = backendOpIP !== "APP_BACKEND_OP_URL" ? backendOpIP : "http://localhost:8000/psifos/api/app"
-backendInfoIp = backendInfoIp !== "APP_BACKEND_INFO_URL" ? backendInfoIp : "http://localhost:8001/psifos/api/public"
+backendOpIP = backendOpIP !== "APP_BACKEND_OP_URL" ? backendOpIP : "http://localhost:8000"
+backendInfoIp = backendInfoIp !== "APP_BACKEND_INFO_URL" ? backendInfoIp : "http://localhost:8001"
 frontIP = frontIP !== "APP_FRONTEND_URL" ? frontIP : "http://localhost:3000/psifos"
 
 export { backendOpIP, backendInfoIp, frontIP };
