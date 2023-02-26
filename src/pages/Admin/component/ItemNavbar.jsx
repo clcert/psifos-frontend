75ba987695ef0a0a6a83675de4b743541f9950aa
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-02-26 14:25:53 -0300

    Refactor item subnavbar admin

---
+++ src/pages/Admin/component/ItemNavbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function ItemNavbar({ active, to, icon, itemText }) {
+  return (
+    <div className="content-subnavbar d-md-flex justify-content-center">
+      <Link
+        className={active !== 0 ? "text-subnavbar" : "text-subnavbar-active"}
+        to={to}
+      >
+        <i className={icon + " d-md-flex justify-content-center"}></i>
+        <span className="">{itemText}</span>
+      </Link>
+    </div>
+  );
+}
--- src/pages/Admin/component/SubNavbar.jsx
+++ src/pages/Admin/component/SubNavbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import ItemNavbar from "./ItemNavbar";
 
 function SubNavbar(props) {
   const { uuid } = useParams();
@@ -9,97 +10,48 @@ function SubNavbar(props) {
       <div className="subnavbar columns is-flex is-centered">
         <div className="subnavbar-inner pt-2">
           <div className="container subnavbar-items columns is-centered pb-2 pt-2">
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 0
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to="/psifos/admin/home"
-              >
-                <i className="fa fa-home mr-2"></i>
-                <span>Votaciones</span>
-              </Link>
-            </div>
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 1
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to={"/psifos/admin/" + uuid + "/panel"}
-              >
-                <i className="fa-solid fa-screwdriver-wrench mr-2"></i>
-                <span>Panel</span>
-              </Link>
-            </div>
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 2
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to={"/psifos/admin/" + uuid + "/resumen"}
-              >
-                <i className="fa fa-list-alt mr-2"></i>
-                <span>Resumen</span>
-              </Link>
-            </div>
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 3
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to={"/psifos/admin/" + uuid + "/voters-list"}
-              >
-                <i className="fa-solid fa-check-to-slot mr-2"></i>
-                <span>Padrón</span>
-              </Link>
-            </div>
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 4
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to={"/psifos/admin/" + uuid + "/trustee"}
-              >
-                <i className="fa-solid fa-user-check mr-2"></i>
-                <span>Custodio de Claves</span>
-              </Link>
-            </div>
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 5
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to={"/psifos/admin/" + uuid + "/resultado"}
-              >
-                <i className="fa-solid fa-square-poll-vertical mr-2"></i>
-                <span>Resultados</span>
-              </Link>
-            </div>
-            <div className="content-subnavbar">
-              <Link
-                className={
-                  props.active !== 6
-                    ? "text-subnavbar"
-                    : "text-subnavbar-active"
-                }
-                to={"/psifos/admin/" + uuid + "/statistics"}
-              >
-                <i className="fa-solid fa-chart-line mr-2"></i>
-                <span>Estadísticas</span>
-              </Link>
-            </div>
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/home"}
+              icon={"fa fa-home"}
+              itemText="Votaciones"
+            />
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/" + uuid + "/panel"}
+              icon="fa-solid fa-screwdriver-wrench"
+              itemText="Panel"
+            />
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/" + uuid + "/resumen"}
+              icon="fa fa-list-alt"
+              itemText="Resumen"
+            />
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/" + uuid + "/voters-list"}
+              icon="fa-solid fa-check-to-slot"
+              itemText="Padrón"
+            />
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/" + uuid + "/trustee"}
+              icon="fa-solid fa-user-check"
+              itemText="Custodios"
+            />
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/" + uuid + "/resultado"}
+              icon="fa-solid fa-square-poll-vertical"
+              itemText="Resultados"
+            />
+            <ItemNavbar
+              active={props.active}
+              to={"/psifos/admin/" + uuid + "/statistics"}
+              icon="fa-solid fa-chart-line"
+              itemText="Estadísticas"
+            />
           </div>
         </div>
       </div>
