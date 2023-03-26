0f695405bc1637742b9f86f6e03cdf407fb25f1d
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-03-26 15:30:07 -0300

    Update item navbar for booth view

--- src/pages/Admin/component/ItemNavbar.jsx
+++ src/pages/Admin/component/ItemNavbar.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 
-export default function ItemNavbar({ active, to, icon, itemText }) {
+export default function ItemNavbar({
+  active,
+  to,
+  icon,
+  itemText,
+  actionClick,
+}) {
   return (
     <div className="content-subnavbar d-md-flex justify-content-center">
       <Link
         className={active ? "text-subnavbar" : "text-subnavbar-active"}
         to={to}
+        onClick={actionClick}
       >
         <i className={icon + " d-md-flex justify-content-center"}></i>
         <span className="">{itemText}</span>
