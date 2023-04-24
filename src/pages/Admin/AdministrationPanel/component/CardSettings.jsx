2c7add13d06c519a9280c96755b15c58ce36ccdf
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-04-24 18:32:36 -0400

    adding bundle file in card admin

--- src/pages/Admin/AdministrationPanel/component/CardSettings.jsx
+++ src/pages/Admin/AdministrationPanel/component/CardSettings.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { backendOpIP } from "../../../../server";
+import { backendInfoIp, backendOpIP } from "../../../../server";
 
 function CardSettings(props) {
-
   /** @state {bool} state for show infor message about copy */
   const [showCopyMessage, setShowCopyMessage] = useState(false);
 
+  /** @urlParam {string} uuid of election  */
   const { uuid } = useParams();
+
+  const bundleDownload = async () => {
+    const url = backendInfoIp + "/election/" + uuid + "/bundle-file";
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 200) {
+      const jsonResponse = await response.json();
+      const bundleContent = JSON.stringify(jsonResponse);
+      let hiddenElement = document.createElement("a");
+      hiddenElement.download = `bundle-file.json`;
+      const blob = new Blob([bundleContent], {
+        type: "application/json",
+      });
+      hiddenElement.href = window.URL.createObjectURL(blob);
+      hiddenElement.click();
+    }
+  };
+
   return (
     <div className="box ">
       <div className="is-size-4">Opciones de la elección</div>
@@ -17,7 +39,9 @@ function CardSettings(props) {
 
       <div className="is-size-6">
         <div className="content-card-admin">
-          <i className="fa-solid fa-pen-to-square pr-1 mr-2"></i>
+          <div className="icon-card-admin d-inline-flex justify-content-center mr-2">
+            <i className="fa-solid fa-pen-to-square"></i>
+          </div>
           <Link
             className="link-without-line"
             to={"/psifos/admin/" + uuid + "/edit-election/"}
@@ -26,7 +50,9 @@ function CardSettings(props) {
           </Link>
         </div>
         <div className="content-card-admin">
-          <i className="fa-solid fa-circle-question pr-2 mr-1"></i>
+          <div className="icon-card-admin d-inline-flex mr-2">
+            <i className="fa-solid fa-circle-question"></i>
+          </div>
           <Link
             className="link-without-line"
             to={"/psifos/admin/" + uuid + "/create-question/"}
@@ -37,7 +63,9 @@ function CardSettings(props) {
 
         {props.election.questions !== null && (
           <div className="content-card-admin">
-            <i className="fa-solid fa-person-booth mr-2"></i>
+            <div className="icon-card-admin d-inline-flex justify-content-center mr-2">
+              <i className="fa-solid fa-person-booth"></i>
+            </div>
             <Link
               className="link-without-line"
               to={"/psifos/admin/" + uuid + "/booth"}
@@ -48,7 +76,9 @@ function CardSettings(props) {
         )}
 
         <div className="content-card-admin">
-          <i className="fa-solid fa-square-poll-horizontal pr-2 "></i>{" "}
+          <div className="icon-card-admin d-inline-flex justify-content-center mr-2">
+            <i className="fa-solid fa-square-poll-horizontal"></i>{" "}
+          </div>
           <Link
             target="_blank"
             className="link-without-line"
@@ -59,7 +89,9 @@ function CardSettings(props) {
         </div>
 
         <div className="content-card-admin">
-          <i className="fa-solid fa-check-to-slot mr-2"></i>
+          <div className="icon-card-admin d-inline-flex justify-content-center mr-2">
+            <i class="fa-solid fa-copy"></i>{" "}
+          </div>
           <CopyToClipboard
             text={backendOpIP + "/" + uuid + "/vote"}
             onCopy={() => setShowCopyMessage(true)}
@@ -74,8 +106,20 @@ function CardSettings(props) {
             </span>
           </CopyToClipboard>
         </div>
+        {props.election.election_status === "Decryptions combined" && (
+          <div onClick={bundleDownload} className="content-card-admin">
+            <div className="icon-card-admin d-inline-flex justify-content-center mr-2">
+              <i class="fa-solid fa-file-arrow-down"></i>{" "}
+            </div>
+            <Link className="link-without-line" to="">
+              Descargar archivo de verificación
+            </Link>
+          </div>
+        )}
         <div className="content-card-admin">
-          <i className="fa-solid fa-trash pr-2 mr-1"></i>
+          <div className="icon-card-admin d-inline-flex justify-content-center mr-2">
+            <i className="fa-solid fa-trash "></i>
+          </div>
           <Link
             className="link-without-line-red"
             to=""
