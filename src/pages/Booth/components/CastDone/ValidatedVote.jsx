d5056658103ba20917c1f775cd02dc34b7860e62
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2023-05-29 11:09:15 -0400

    FEATURE: Update ValidatedVote view style
    Inform the voter that the process is over and that they have the option to make certain choices.

--- src/pages/Booth/components/CastDone/ValidatedVote.jsx
+++ src/pages/Booth/components/CastDone/ValidatedVote.jsx
@@ -1,6 +1,47 @@
 import { useParams } from "react-router-dom";
 import { backendOpIP, frontIP } from "../../../../server";
 
+function OptinalStep({title, text}) {
+  return(
+    <div>
+      <div className="mb-1" style={{fontSize:"23px"}}>
+        <i class="fa fa-check mr-3 is-color-blue" aria-hidden="true"/>
+        <span>{title}</span>
+      </div>
+      <p>{text}</p>
+    </div>
+  )
+}
+
+function PrettyButton({
+  onClick, icon, text, id
+}) {
+  return (
+    <button
+      className="button"
+      onClick={onClick}
+      id={id}
+    >
+      <span className="icon is-small">
+        {icon}
+      </span>
+      <span>{text}</span>
+    </button>
+  )
+}
+
+function InfoCard({title, content, buttons}) {
+  return (
+    <div className="box">
+      <p className="subtitle is-3 is-color-blue">
+        {title}
+      </p>
+      {content}
+      {buttons}
+    </div>
+  )
+}
+
 function ValidatedVote(props) {
   const { shortName } = useParams();
   async function downloadFile() {
@@ -41,71 +82,88 @@ function ValidatedVote(props) {
   }
 
   return (
-    <>
-      <p className="subtitle is-3 has-text-black mb-1">
-        HEMOS RECIBIDO TU VOTO ENCRIPTADO
-      </p>
-      <p className="subtitle has-text-black send-text">
-        Tu voto encriptado ha sido recibido y validado exitosamente
-      </p>
-
-      <div className="columns">
-        <div className="column castdone-box mb-0 is-5">
-          <button
-            className="button"
-            onClick={downloadFile}
-            id="back-vote-button"
-          >
-            <span className="icon is-small is-size-7-touch">
-              <i className="fa-solid fa-file-arrow-down"></i>
-            </span>
-            <span className="is-size-7-touch">
-              DESCARGAR CERTIFICADO DE VOTO
-            </span>
-          </button>
-          <p className="subtitle is-6 mt-3">
-            El certificado de voto no permite conocer como votaste, sino que
-            solamente acredita que tu voto fue realizado correctamente y será
-            contabilizado en el escrutinio final.
-          </p>
-        </div>
-
-        <div className="column is-2"></div>
-
-        <div className="column castdone-box is-5">
-          <button
-            className="button"
-            onClick={openBallotBox}
-            id="back-vote-button"
-          >
-            <span className="icon is-small">
-              <i className="fa-solid fa-box-archive"></i>
-            </span>
-            <span>VER URNA ELECTRÓNICA</span>
-          </button>
-          <p className="subtitle is-6 mt-3">
-            La urna electrónica contiene todos los votos encriptados enviados hasta
-            el momento.
-          </p>
-        </div>
-      </div>
-
-      <p className="subtitle is-5 pb-2 mt-4">
-        Si lo deseas, puedes volver a votar durante el tiempo que la votación
-        esté abierta. Si lo haces, el nuevo voto encriptado reemplazará al existente, y
-        sólo se contabilizará el último voto encriptado emitido.
-      </p>
-      <button
-        onClick={exit}
-        className="button is-medium my-4"
-        id="back-vote-button"
-      >
-        <span className="icon is-small">
-          <i className="fas fa-2x fa-caret-left"></i>
-        </span>
-        <span>SALIR</span>
-      </button>
-    </>
+    <div className="validated-vote">
+      <InfoCard 
+        title="SU PROCESO DE VOTACIÓN HA FINALIZADO"
+        content={
+          <div>
+            <div className="mb-4" style={{fontSize:"23px"}}>
+              <span>
+                Su voto encriptado ha sido recibido exitosamente.
+              </span>
+            </div>
+          </div>
+        }
+        buttons={
+          <div>
+            <PrettyButton
+              onClick={exit}
+              id="back-vote-button"
+              icon={
+                <i class="fa-solid fa-house" style={{fontSize: "18px"}}/>
+              }
+              text="VOLVER AL INICIO"
+            />
+          </div>
+        }
+      />
+      <InfoCard
+        title="¿QUÉ PUEDO HACER AHORA?"
+        content={
+          <div className="text-align-justify mb-5">
+            <OptinalStep
+              title="Puede descargar el certificado de su voto."
+              text={
+                "Este documento acredita que tu voto fue realizado " +
+                "correctamente y que será contabilizado en el escrutinio final. " +
+                "Sin embargo, el certificado no proporciona información sobre " +
+                "las preferencias emitidas en el voto."
+              }
+              />
+            <OptinalStep
+              title="Puede verificar su voto en la urna electrónica."
+              text={
+                "En este lugar se encuentran todos los votos encriptados " +
+                "y enviados hasta el momento. Al ingresar, podrá observar que la " +
+                "fila resaltada corresponde a una muestra de su voto encriptado."
+              }
+              />
+            <OptinalStep
+              title="Si lo desea, tiene la opción de volver a votar mientras el proceso esté abierto."
+              text={
+                "En caso de hacerlo, su nuevo voto encriptado reemplazará al anterior, y solo se " +
+                "tomará en cuenta el último voto encriptado emitido."
+              }
+              />
+          </div>
+        }
+        buttons={
+          <div className="columns">
+            <div className="column castdone-box mb-0 is-5">
+              <PrettyButton
+                id="back-vote-button"
+                onClick={downloadFile}
+                text="DESCARGAR CERTIFICADO DE VOTO"
+                icon={
+                  <i className="fa-solid fa-file-arrow-down"/>
+                }
+              />  
+            </div>
+            <div className="column is-2"/>
+            <div className="column castdone-box is-5">
+              <PrettyButton
+                onClick={openBallotBox}
+                id="back-vote-button"
+                icon={
+                  <i className="fa-solid fa-box-archive"/>
+                }
+                text="VER URNA ELECTRÓNICA"
+              /> 
+            </div>
+          </div>
+        }
+      />
+    </div>
   );
 }
 
