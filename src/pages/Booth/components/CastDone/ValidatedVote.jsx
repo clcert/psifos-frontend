eaf02d0b47b48ce649a78f6405a5be8af19fc963
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-06-04 23:36:04 -0400

    add feedback to download bundle

--- src/pages/Booth/components/CastDone/ValidatedVote.jsx
+++ src/pages/Booth/components/CastDone/ValidatedVote.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { backendOpIP, frontIP } from "../../../../server";
 
-function OptinalStep({title, text, button}) {
-  return(
+function OptinalStep({ title, text, button }) {
+  return (
     <div className="optional-step">
       <div className="statement mb-1 text-align-justify">
-        <i class="fa fa-check mr-3 is-color-blue opt-title" aria-hidden="true"/>
+        <i
+          class="fa fa-check mr-3 is-color-blue opt-title"
+          aria-hidden="true"
+        />
         <span>{title}</span>
       </div>
       <p className="text text-align-justify">{text}</p>
@@ -13,40 +17,41 @@ function OptinalStep({title, text, button}) {
         {button}
       </div>
     </div>
-  )
+  );
 }
 
-function PrettyButton({
-  onClick, icon, text, id
-}) {
+function PrettyButton({ onClick, icon, text, id, subContent }) {
   return (
-    <button
-      className="button"
-      onClick={onClick}
-      id={id}
-    >
-      <span className="icon is-small">
-        {icon}
-      </span>
-      <span>{text}</span>
-    </button>
-  )
+    <div>
+      <button className="button" onClick={onClick} id={id}>
+        <span className="icon is-small">{icon}</span>
+        <span>{text}</span>
+      </button>
+      {subContent}
+    </div>
+  );
 }
 
-function InfoCard({title, content}) {
+function InfoCard({ title, content }) {
   return (
     <div className="box">
-      <p className="subtitle is-3 is-color-blue">
-        {title}
-      </p>
+      <p className="subtitle is-3 is-color-blue">{title}</p>
       {content}
     </div>
-  )
+  );
 }
 
 function ValidatedVote(props) {
+  const [loadFileContent, setLoadFileContent] = useState();
+
   const { shortName } = useParams();
   async function downloadFile() {
+    setLoadFileContent(
+      <p className="mt-1">
+        Generando el archivo{" "}
+        <i id="step_1" className={"fa-solid fa-spinner fa-spin"}></i>
+      </p>
+    );
     const url = backendOpIP + "/" + shortName + "/get-certificate";
     const resp = await fetch(url, {
       method: "GET",
@@ -63,6 +68,12 @@ function ValidatedVote(props) {
       lnk.href = objURL;
       lnk.download = fname;
       lnk.click();
+      setLoadFileContent(
+        <p className="mt-1">
+          Archivo descargado{" "}
+          <i id="step_1" className={"fa-solid fa-circle-check"}></i>
+        </p>
+      );
       setTimeout(() => {
         window.URL.revokeObjectURL(objURL);
       }, 250);
@@ -85,20 +96,18 @@ function ValidatedVote(props) {
 
   return (
     <div className="validated-vote">
-      <InfoCard 
+      <InfoCard
         title="SU PROCESO DE VOTACIÓN HA FINALIZADO"
         content={
           <div>
-            <div className="mb-4" style={{fontSize:"23px"}}>
-              <span>
-                Su voto encriptado ha sido recibido exitosamente.
-              </span>
+            <div className="mb-4" style={{ fontSize: "23px" }}>
+              <span>Su voto encriptado ha sido recibido exitosamente.</span>
             </div>
             <PrettyButton
               onClick={exit}
               id="back-vote-button"
               icon={
-                <i class="fa-solid fa-house" style={{fontSize: "18px"}}/>
+                <i class="fa-solid fa-house" style={{ fontSize: "18px" }} />
               }
               text="VOLVER AL INICIO"
             />
@@ -121,11 +130,10 @@ function ValidatedVote(props) {
                 <PrettyButton
                   onClick={downloadFile}
                   id="back-vote-button"
-                  icon={
-                    <i className="fa-solid fa-file-arrow-down"/>
-                  }
+                  icon={<i className="fa-solid fa-file-arrow-down" />}
                   text="DESCARGAR CERTIFICADO DE VOTO"
-                /> 
+                  subContent={loadFileContent}
+                />
               }
             />
             <OptinalStep
@@ -137,20 +145,19 @@ function ValidatedVote(props) {
               }
               button={
                 <PrettyButton
-                onClick={openBallotBox}
-                id="back-vote-button"
-                icon={
-                  <i className="fa-solid fa-box-archive"/>
-                }
-                text="VER URNA ELECTRÓNICA"
-              />
+                  onClick={openBallotBox}
+                  id="back-vote-button"
+                  icon={<i className="fa-solid fa-box-archive" />}
+                  text="VER URNA ELECTRÓNICA"
+                />
               }
-              />
-              <p className="optional-note">
-                ¡Importante! Si así lo desea, tiene la opción de volver a votar mientras el proceso
-                se mantenga abierto. En caso de que decida hacerlo, su nuevo voto encriptado reemplazará
-                al anterior y solo se considerará válido el último voto encriptado emitido.
-              </p>
+            />
+            <p className="optional-note">
+              ¡Importante! Si así lo desea, tiene la opción de volver a votar
+              mientras el proceso se mantenga abierto. En caso de que decida
+              hacerlo, su nuevo voto encriptado reemplazará al anterior y solo
+              se considerará válido el último voto encriptado emitido.
+            </p>
           </div>
         }
       />
