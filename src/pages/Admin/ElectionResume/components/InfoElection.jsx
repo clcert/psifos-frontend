3a0abb776ede6084c035cb0a9e605a213bb21bb0
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2023-07-08 16:36:50 -0400

    FEATURE limit info on resume
    Ponderation table only must be present if max weight > 1

--- src/pages/Admin/ElectionResume/components/InfoElection.jsx
+++ src/pages/Admin/ElectionResume/components/InfoElection.jsx
@@ -1,6 +1,16 @@
 import ResumeTable from "./ResumeTable";
 import WeightsTable from "./WeightsTable";
 
+function StyledTitle({ title }) {
+  return (
+    <div>
+      <h1 className="title is-size-4 pt-4">
+        {title}
+      </h1>
+    </div>
+  )
+}
+
 function InfoElection(props) {
   /**
    * Component containing the election information
@@ -14,20 +24,20 @@ function InfoElection(props) {
             {" "}
             {props.weightsEnd && props.weightsInit ? (
               <>
-                <div>
-                  <h1 className="title is-size-4">Apertura de Urna</h1>
-                </div>
-                <ResumeTable />
-                <div>
-                  <h1 className="title is-size-4 pt-4">
-                    Número de votantes por ponderación
-                  </h1>
-                </div>
-                <WeightsTable
-                  weightsInit={props.weightsInit}
-                  weightsEnd={props.weightsEnd}
-                  weightsElection={props.weightsElection}
-                />
+                <>
+                  <StyledTitle title="Apertura de Urna"/>
+                  <ResumeTable />
+                </>
+                {props.maxWeight && props.maxWeight !== 1 && (
+                  <>
+                    <StyledTitle title="Número de votantes por ponderación"/>
+                    <WeightsTable
+                      weightsInit={props.weightsInit}
+                      weightsEnd={props.weightsEnd}
+                      weightsElection={props.weightsElection}
+                    />
+                  </>
+                )}
               </>
             ) : (
               <div className="box" id="not-results-box">
