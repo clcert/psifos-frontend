bdeb73a4d858a9b7b2bb69488d192282003fc657
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-11-12 15:06:53 -0300

    Fix formatting and download key duplication in Keygenerator.jsx

--- src/pages/Admin/CustodioClaves/components/StepButton.jsx
+++ src/pages/Admin/CustodioClaves/components/StepButton.jsx
@@ -4,7 +4,7 @@ function StepButton(props) {
   return (
     <Link
       id={props.id}
-      className="is-large button is-info is-light is-outlined py-2 my-3"
+      className="is-size-6-mobile is-large button is-info is-light is-outlined py-2 my-3"
       style={{
         textDecoration: "None",
         textTransform: "uppercase",
