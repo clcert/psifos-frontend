a3693e8a927e1d3dd02a65950adf3a5a3c3f7aed
JaramilloCris <jaramillo.cristobal.a@gmail.com>
2024-03-17 22:30:45 -0300

    Actualizar componente InputRadio.jsx en la sección de preguntas de la página de elección en Booth.

--- src/pages/Booth/Election/QuestionSection/Questions/InputRadio.jsx
+++ src/pages/Booth/Election/QuestionSection/Questions/InputRadio.jsx
@@ -1,24 +1,19 @@
 import { useSelector } from "react-redux";
 export function OptionInputRadio({
-  optionId, questionId, optionLabel,
-  isSelected, inputHandler, isBordered=false,
+  optionId,
+  questionId,
+  optionLabel,
+  isSelected,
+  inputHandler,
+  isBordered = false,
 }) {
   return (
-    <div
-      key={optionId}
-      className={`mt-2 ${isBordered && "is-bordered"}`}
-      style={{padding: "8px"}}
-    >
+    <div key={optionId} className="mt-2">
       <label
         key={optionId}
         className={
-          "d-inline-flex align-items-center radio " +
-          "question-answer question-answer-enabled px-3 py-2 " +
-          `${isSelected && (
-            `radio-button-selected ${
-              !isBordered && "is-bordered"
-            }`
-          )} `
+          "d-inline-flex align-items-center radio question-answer question-answer-enabled px-3 py-2 " +
+          (isSelected ? "answer-selected" : "")
         }
       >
         <input
@@ -28,21 +23,22 @@ export function OptionInputRadio({
           name={"answer_" + questionId}
           value={optionId}
           onClick={inputHandler}
-          checked={isSelected}
         />
         <span className="is-size-5">{optionLabel}</span>
       </label>
     </div>
-  )
+  );
 }
 
-
 function InputRadio({
-  setAnswers, setBlankButton, setNullButton,
-  addAnswer, question, questionId,
+  setAnswers,
+  setBlankButton,
+  setNullButton,
+  addAnswer,
+  question,
+  questionId,
 }) {
-
-  const { include_blank_null, closed_options } = question
+  const { include_blank_null, closed_options } = question;
   const includeInformalAns = include_blank_null === "True";
 
   let answers = useSelector((state) => state.booth.answers)[questionId];
@@ -54,9 +50,7 @@ function InputRadio({
   return (
     <div>
       {closed_options.map((key, index) => {
-        if (
-          !includeInformalAns || index < closed_options.length - 2
-        )
+        if (!includeInformalAns || index < closed_options.length - 2)
           return (
             <OptionInputRadio
               optionId={index}
