1b27facf99c4d0c16af89631947e3c051e5b78ce
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2024-05-22 15:11:49 -0400

    style: improves the formal options logic into the mn booth
    Generalizes the formal options selector. Now it can be used on any part of the front.
    Defines the logic behind each mn selector on its component.

---
+++ src/component/Selector/AsyncSelector.jsx
@@ -0,0 +1,34 @@
+import AsyncSelect from "react-select/async";
+
+const colors = {
+    disableGray: "#bbc1c6",
+    lightBlue: "#0095d4",
+    lightOrange: "#FFE8DF",
+}
+
+export default function AsyncSelector (props) {
+    return (
+        <AsyncSelect
+            {...props}
+            styles={{
+                control: (baseStyles) => ({
+                    ...baseStyles,
+                    backgroundColor:
+                    isDisabled ? colors.disableGray : "white",
+                }),
+                groupHeading: (provided, state) => ({
+                    ...provided,
+                    backgroundColor: colors.lightBlue,
+                    color: "white",
+                    padding: "10px 10px",
+                    display: "flex",
+                    fontSize: "14px",
+                }),
+                option: (base, { isFocused }) => ({
+                    ...base,
+                    backgroundColor: isFocused ? colors.lightOrange : undefined,
+                }),
+            }}
+        />
+    )
+}
