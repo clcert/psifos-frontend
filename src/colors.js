09e4e060723e5750bfcec4cac7e28b2bd3637b5a
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2024-06-11 12:38:13 -0400

    style: organizes the global constants
    There where to many colors into the constants file, so now they have their own file, and you can see clearly what are the colors and what are the other constants.
    Some colors names where updated too, to see clearly the difference between all the available colors.

---
+++ src/colors.js
@@ -0,0 +1,31 @@
+export const principalBlue = "#0E4B90";
+export const transparentPrincipalBlue = "rgb(14, 75, 144, 0.5)";
+export const principalLightBlue = "#84C8EF";
+export const mediumBlue = "#0095d4";
+
+export const disabledGray = "#bbc1c6";
+
+export const lightOrange = "#FFE8DF";
+
+export const gradientPalettes = {
+  1: [principalBlue],
+  2: [principalBlue, principalLightBlue],
+  3: [
+    principalBlue,
+    "#498AC0",
+    principalLightBlue
+  ],
+  4: [
+    principalBlue,
+    "#3575B0",
+    "#5D9ECF",
+    principalLightBlue
+  ],
+  5: [
+    principalBlue,
+    "#2C6AA8",
+    "#498AC0",
+    "#67A9D7",
+    principalLightBlue
+  ],
+}
--- src/constants.js
+++ src/constants.js
@@ -135,31 +135,3 @@ export const tallyNames = [
   ...firstMajorityTallyNames,
   ...preferentialRankingTallyNames,
 ];
-
-export const principalBlue = "#0E4B90";
-export const transparentPrincipalBlue = "rgb(14, 75, 144, 0.5)";
-
-export const principalLightBlue = "#84C8EF";
-
-export const gradientPalettes = {
-  1: [principalBlue],
-  2: [principalBlue, principalLightBlue],
-  3: [
-    principalBlue,
-    "#498AC0",
-    principalLightBlue
-  ],
-  4: [
-    principalBlue,
-    "#3575B0",
-    "#5D9ECF",
-    principalLightBlue
-  ],
-  5: [
-    principalBlue,
-    "#2C6AA8",
-    "#498AC0",
-    "#67A9D7",
-    principalLightBlue
-  ],
-}
